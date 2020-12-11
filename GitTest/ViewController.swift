70858a41f1f1612ae353a475520769873d728d69
Yi-Huei <e095279@gmail.com>
2020-12-11 23:03:27 +0800

    1.加入方法reverse(text: String) 2.viewDidLoad()加入print(reverse(text: "stressed"))

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let message = "Hello Wold!"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(message)
+        print(reverse(text: "stressed"))
     }
 
-
+    func reverse(text: String) -> String {
+        return String(text.reversed())
+    }
 }
 
